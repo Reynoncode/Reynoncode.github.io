41b8f1365fd8d6ddb1c5cab7f9ae1973c3545455
Almoda <cfrovramin@gmail.com>
2026-04-11 12:11:57 +0400

    Add vendorId to cart item object

--- js/cart.js
+++ js/cart.js
@@ -50,6 +50,10 @@ const cart = {
         size: size || null, quantity: 1,
         addedAt:   firebase.firestore.FieldValue.serverTimestamp(),
         updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+
+          vendorId: product.vendorId || product.userId || product.sellerId || null,
+        addedAt:  firebase.firestore.FieldValue.serverTimestamp(),
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
       });
     }
 
